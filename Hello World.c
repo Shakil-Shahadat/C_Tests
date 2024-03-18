2b5701e58b5f03c1dd6f1f7ef736ab425bb58159
Shakil Shahadat <shakil.regi@gmail.com>
2024-03-19 05:16:36 +0600

    Create Hello World.c

---
+++ Hello World.c
@@ -0,0 +1,9 @@
+#include<stdio.h> // .h is required
+// Using namespace std; // Produces error
+
+int main()
+{
+	printf( "Hello World!" );
+
+	return 0; // Return is optional but requires a numeric value
+}
