c79be92ee7602830eae1e6fa20eea74d9c0aaa33
Shakil Shahadat <shakil.regi@gmail.com>
2024-03-20 03:41:35 +0600

    Update 03. Hello World.c

--- Basic/03. Hello World.c
+++ Basic/03. Hello World.c
@@ -3,7 +3,9 @@
 
 int main() // void / int is required. Exactly 'main' has to be written
 {
-	printf( "Hello World!" ); // Double quotation && semi-colon are required
+	printf( "Hello World!" );
+	// Exactly 'printf' has to be written
+	// Double quotation && semi-colon are required
 
 	// return 0;
 	// Return is optional but requires a numeric value if main is of type int
