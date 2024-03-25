f2e1690760240732d56b744522bceddfbed1b54a
Shakil Shahadat <shakil.regi@gmail.com>
2024-03-25 08:59:27 +0600

    Create 23. Exercise 1-9 v2.c

---
+++ Book Examples/The C Programming Language/01. A Tutorial Introduction/23. Exercise 1-9 v2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int c;
+
+	while( ( c = getchar() ) != EOF )
+	{
+		putchar( c );
+		if( c == ' ' )
+		{
+			while( ( c = getchar() ) == ' ' )
+			{}
+			putchar( c );
+		}
+	}
+}
