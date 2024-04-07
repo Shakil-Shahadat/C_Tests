82db5302e52658689874bd875413b1a03ebab946
Shakil Shahadat <shakil.regi@gmail.com>
2024-04-07 11:30:16 +0600

    Update 20. Character Constant.c

--- Book Examples/The C Programming Language/01. A Tutorial Introduction/20. Character Constant.c
+++ Book Examples/The C Programming Language/01. A Tutorial Introduction/20. Character Constant.c
@@ -2,5 +2,14 @@
 
 int main()
 {
-	printf( "%d", 'A' ); // Find character set value of A
+	// Ref: https://www.ascii-code.com/
+
+	printf( "%d\n", 'A' ); // Find character set 'value' of A
+	printf( "%c\n", 65 ); // Find character set 'alphabet' for 65
+	// printf( "%c\n", 7 ); // This creates an auditory warning
+
+	for( int i = 32; i < 128; i++ )
+	{
+		printf( "\nThe ASCII value of %c is %d", i, i ); // ASCII Printable Characters
+	}
 }
